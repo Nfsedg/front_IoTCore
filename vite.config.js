b154069e4244fbd748dd8983546ceed74f120238
Edgar Pech Garcia <71201852+Nfsedg@users.noreply.github.com>
2024-08-06 14:02:47 -0500

    Fix port

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 80,
-    strictPort: true,
-  },
   server: {
     host: true,
     strictPort: true,
     port: 80,
+    watch: {
+      usePolling: true,
+    },
   },
 });
